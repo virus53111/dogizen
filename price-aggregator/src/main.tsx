f36d60ba199a32b09ac58d5cb08339396dc03046
virus53111 <151654868+virus53111@users.noreply.github.com>
2026-09-21 20:11:32 +0300

    Switch CenaRadar to premium app

--- price-aggregator/src/main.tsx
+++ price-aggregator/src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
-import './enhancements.css';
+import PremiumApp from './PremiumApp';
+import './premium.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PremiumApp />
   </StrictMode>,
 );
