dc23b246839ddd2014003269a3736b5aa7849456
virus53111 <151654868+virus53111@users.noreply.github.com>
2026-09-21 21:04:53 +0300

    Wire language guard into CenaRadar

--- price-aggregator/src/main.tsx
+++ price-aggregator/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import PremiumApp from './PremiumApp';
+import LanguageGuard from './LanguageGuard';
 import './premium.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <LanguageGuard />
     <PremiumApp />
   </StrictMode>,
 );
