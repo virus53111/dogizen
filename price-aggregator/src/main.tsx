a2168c17ad3eae64a85efbdf9e796d2a781c2d6c
virus53111 <151654868+virus53111@users.noreply.github.com>
2026-09-21 22:26:30 +0300

    Show Ottocast product feed in CenaRadar

--- price-aggregator/src/main.tsx
+++ price-aggregator/src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import PremiumApp from './PremiumApp';
 import LanguageGuard from './LanguageGuard';
+import OttocastFeed from './OttocastFeed';
 import './premium.css';
+import './ottocast-feed.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LanguageGuard />
     <PremiumApp />
+    <OttocastFeed />
   </StrictMode>,
 );
